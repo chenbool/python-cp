6d8be6f017b9f3d13cb97aa9cc4e39ac2169ae63
bool <81001985@qq.com>
2019-02-13 11:03:17 +0800

    *

--- db.py
+++ db.py
@@ -28,6 +28,8 @@ class Train(object):
         # 列表网址
         cp_list = tree.xpath('//a[@class="lot-list"]/@href')
 
+        # cp_list = ['/k/kjls/dfc-gxklsc.html']
+
         # gevent.joinall([gevent.spawn(self.get_content, '/k/index.html')])
         temp = []
         # 遍历
@@ -42,7 +44,6 @@ class Train(object):
     # 获取详情页
     def get_info(self, url):
         tree = self.get_content(url)
-        # //*[@id="kj"]/div[2]/div
         # 名称
         kaij_name = tree.xpath('//*[@id="kj"]/div[2]/div/div[@class="kaij-title"]/span[@class="kaij-name"]/text()')
         # 期号
@@ -78,25 +79,27 @@ class Train(object):
     def get_content(self, url):
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
         proxies = {
-            'http': 'http://183.148.148.81:9999',
             'http': 'http://121.61.2.56:9999',
-            'http': 'http://116.208.55.173:9999',
-            'http': 'http://115.151.5.177:9999',
-            'http': 'http://110.52.235.139:9999',
-            'http': 'http://112.85.165.66:9999',
-            'http': 'http://116.209.59.174:9999',
-            'http': 'http://121.61.1.9:9999',
-            'http': 'http://180.119.141.144:9999',
-            'http': 'http://116.209.54.168:9999',
-            'http': 'http://116.209.58.45:9999',
-            'http': 'http://125.123.142.215:9999',
-            'http': 'http://110.52.235.196:9999',
-            # 'https': 'https://112.98.126.98:37614'
+            'http': 'http://116.208.55.173:9999'
         }
-        page = requests.get(self.baseUrl+url, headers=headers, proxies=proxies, timeout=5)
-        # page = page.text.strip();
-        tree = page.text.replace('\r\n', '')  # 替换成空格
-        return html.fromstring(tree)
+
+        try:
+            # timeout=([连接超时时间], [读取超时时间])
+            page = requests.get(self.baseUrl+url, headers=headers, proxies=proxies, timeout=(10, 3))
+            tree = page.text.replace('\r\n', '')  # 替换成空格
+            return html.fromstring(tree)
+        except requests.exceptions.HTTPError:
+            print('状态异常: ' + self.baseUrl + url)
+        except requests.exceptions.ConnectionError:
+            print('连接异常: '+self.baseUrl+url)
+        except requests.exceptions.InvalidURL:
+            print('网址错误: ' + self.baseUrl + url)
+        except requests.exceptions.ConnectTimeout:
+            print('链接超时: '+self.baseUrl+url)
+        except requests.exceptions.ChunkedEncodingError:
+            print('编码异常: '+self.baseUrl+url)
+        except:
+            print('运行异常: '+self.baseUrl+url)
 
 def main():
     start = time.time()
@@ -104,7 +107,7 @@ def main():
     train.run()
     # 计算执行时间
     end = time.time()
-    print('执行时间: %f' %(end-start))
+    print('执行时间: %f 秒' %(end-start))
     
 if __name__ == "__main__":
     main()
