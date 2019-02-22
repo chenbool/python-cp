3e25bac8faeced675240fc9d60f64ab4f90e383e
bool <81001985@qq.com>
2019-02-22 16:33:01 +0800

    '*'

--- manhua/app.py
+++ manhua/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import gevent
-from gevent import monkey
-monkey.patch_all()
+# import gevent
+# from gevent import monkey
+# monkey.patch_all()
 
 import re
 import os
